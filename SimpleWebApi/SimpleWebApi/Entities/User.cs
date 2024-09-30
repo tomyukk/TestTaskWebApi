924e75b706952e32a87f56c5fe67bd2da9d78cde
tomyukk <tomyukm@gmail.com>
2024-10-01 00:34:46 +0300

    Initial commit

---
+++ SimpleWebApi/SimpleWebApi/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleWebApi.Entities
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordHashSalt { get; set; }
+    }
+}
